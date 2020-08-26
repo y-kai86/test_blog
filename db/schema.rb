d84d206f04ce9834b66ffcccbe5a89e226f64f0c
y-kai86 <y.kai486@gmail.com>
2020-08-26 12:24:49 +0900

    3

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_08_24_132938) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "tite"
+    t.text "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
